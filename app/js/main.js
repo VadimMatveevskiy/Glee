1dbffd667acc5a95ee8c592721fc9533ad73d588
Vadim Matveevskiy <matveevskiy4598@mail.ru>
2022-04-16 18:22:15 +0500

    added shop page and product fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,41 @@
 $(function(){
+
+  $('.shop-content__filter-btn').on('click', function(){
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.product-top__items-item').addClass('product-top__items-item--list');
+  });
+
+  $('.button-grid').on('click', function(){
+    $('.product-top__items-item').removeClass('product-top__items-item--list');
+  });
+
+  $('.select-style').styler()
+
+  $('.star').rateYo({
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    starWidth: "10px",
+    readOnly: true,
+  })
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.01,
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+  },
+  });
+
     $('.slider-top__items').slick({
         dots: true,
         arrows: false,
